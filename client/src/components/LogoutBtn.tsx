c400780e38348c91275e9b7edfc7a2861256a1f9
jask1m <jkim94960@gmail.com>
2024-09-09 21:21:43 -0700

    call logout route in client

--- client/src/components/LogoutBtn.tsx
+++ client/src/components/LogoutBtn.tsx
@@ -1,19 +1,33 @@
 import { useAuthContext } from "../hooks/useAuthContext"
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const LogoutBtn = () => {
-  const { dispatch } = useAuthContext();
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const { user, dispatch } = useAuthContext();
   const navigate = useNavigate();
 
-  const logout = () => {
-    localStorage.removeItem('user');
-    dispatch({ type: 'LOGOUT' });
-    navigate('/user/login');
+  const logout = async () => {
+    try {
+      localStorage.removeItem('user');
+      const response = await axios.post(`${BASE_URL}/user/logout`, {}, { 
+        withCredentials: true,
+        headers: { 
+          Authorization: `Bearer ${user?.accessToken}`,
+        }
+      });
+      if (response.status == 200) {
+        dispatch({ type: 'LOGOUT' });
+        navigate('/user/login');
+      }
+    } catch (error) {
+      console.log('Error logging out:', error);
+    }
   }
 
   return (
-    <button 
-      onClick={logout} 
+    <button
+      onClick={logout}
       className="px-4 py-2 text-black hover:bg-gray-100 rounded-full transition duration-300"
     >
       Logout
