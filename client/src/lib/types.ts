6b7f6f4d836a14134baa3c63bfd3cff08b376b0a
jask1m <jkim94960@gmail.com>
2024-09-13 17:19:50 -0700

    strengthen zod validation

--- client/src/lib/types.ts
+++ client/src/lib/types.ts
@@ -2,18 +2,26 @@ import { z } from 'zod';
 
 export const courseSchema = z.object({
   name: z.string().nonempty("Name is required"),
-  departureAddress: z.string().nonempty("Address is required"),
-  departureCity: z.string().min(1, "City is required").refine(value => isNaN(Number(value)), {
-    message: "City must be a string, not a number"
+  departureAddress: z.string()
+    .regex(/^\d+\s+\S+.*$/, "Address must start with a number, followed by a street name")
+    .nonempty("Address is required"),
+  departureCity: z.string()
+    .min(1, "City is required")
+    .refine(value => /^[a-zA-Z\s'-]+$/.test(value), {
+    message: "City must contain only letters"
   }),
   departureZip: z.string()
     .min(5, "Zipcode must be at least 5 characters")
     .max(10, "Zipcode cannot be more than 10 characters")
     .regex(/^\d{5}(-\d{4})?$/, "Invalid US zipcode format"),
   departureDateTime: z.string().nonempty("Departure date and time is required"),
-  destinationAddress: z.string().nonempty("Address is required"),
-  destinationCity: z.string().min(1, "City is required").refine(value => isNaN(Number(value)), {
-    message: "City must be a string, not a number"
+  destinationAddress: z.string()
+    .regex(/^\d+\s+\S+.*$/, "Address must start with a number, followed by a space and a street name")
+    .nonempty("Address is required"),
+  destinationCity: z.string()
+    .min(1, "City is required")
+    .refine(value => /^[a-zA-Z\s'-]+$/.test(value), {
+    message: "City must contain only letters"
   }),
   destinationZip: z.string()
     .min(5, "Zipcode must be at least 5 characters")
@@ -41,3 +49,5 @@ export const authSignUpFormSchema = authFormSchema.extend({
 });
 
 export type TAuthSignUpFormSchema = z.infer<typeof authSignUpFormSchema>;
+
+export type TCarpoolOptions = "Create" | "View" | "Matches";
